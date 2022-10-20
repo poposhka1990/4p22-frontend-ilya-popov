96a757e48808116cf7bb8874f42a5769560eba8d
poposhka1990 <popovemail1990@gmail.com>
2022-10-20 08:27:19 +0300

    add lessons

---
+++ lesson_3-1/script.js
@@ -0,0 +1,14 @@
+'use strict'
+
+// var someVariable = 'Hello, world!';
+
+// let someVariable = 'Hello World';
+// const someConstant = 'Hello World';
+
+// console.log(someVariable);
+
+const a = 5;
+let b;
+
+// b = 4;
+
