2b9be1c66a5dee75e3595345bc7582b41fcb28e3
poposhka1990 <popovemail1990@gmail.com>
2022-10-23 15:38:58 +0300

    modify script file

--- lesson_3-1/script.js
+++ lesson_3-1/script.js
@@ -7,8 +7,72 @@
 
 // console.log(someVariable);
 
-const a = 5;
-let b;
+// const a = 5;
+// let b;
 
 // b = 4;
 
+
+const num = 5; // number | +-(2^53 - 1)
+console.log(typeof num)
+const floatNum = 5.2; // number | +-(2^53 - 1)
+const bigNum = 9007199254740992; // bigint | число произвольной длины
+
+const str = 'String'; // строка
+const name = 'Hello! I\'m Ilya';
+const quote = "He said: \"I wanna go home\""; 
+
+// Hello! 
+
+const isVisible = true; // boolean
+const isHidden = false; // boolean
+
+// ----------------------------
+
+const someConstant = null; // null
+let someVariable = undefined; // undefined
+
+// symbol
+
+// строковый тип преобразования
+
+const num2 = 5;
+const bool = true;
+
+const strFronNum = '' + num2;  // преобразует в строку
+
+const str2 = String(num);
+
+// числовой тип преобразования
+
+const str3 = '5';
+const bolSecond = true;
+
+const numberFromStr = Number(str);
+ // NaN Not a Number
+
+ +bool
+
+ // булевский тип преобразования
+
+ const strThird = 'hjkhu';
+ const numAgain = 5;
+
+ const boolFromStr = Boolean(strThird)
+
+
+ // falsey-значения 
+ // 0, null, undefined, '', NaN
+
+
+// выражение
+// оператор - + - / бинарные
+// операнды - числа, с которыми производятся операции
+// типы операторов - унарные и бинарные
+// +sum 
+
+// Математика
+// Сокращенная арифметика
+// n++ n-- // инткремент декремент постфикс
+
+// --n ++n (префикс)
