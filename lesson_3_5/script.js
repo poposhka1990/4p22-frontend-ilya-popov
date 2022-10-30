0a8fb1247feac8dfe18aa799ec342e0749f50885
poposhka1990 <popovemail1990@gmail.com>
2022-10-30 16:02:46 +0300

    create lesson_3-5

--- lesson_3_5/script.js
+++ lesson_3_5/script.js
@@ -1,5 +1,36 @@
 'use strict'
 
+// hoisting - всплытие вверх области видимости
+
+sayHello();
+sayHello2();
+
+function sayHello() {
+    console.log('Hello From Declaration');
+}
+
+const sayHello2 = function () {
+    console.log('Hello From Expression');
+}
+
+const sayHello3 = () => {
+    console.log('Hello From Lambda Expresion');
+}
+
+sayHello3();
+
+console.log(x)
+var x = '3';
+
+function printX() {
+    console.log(x);
+
+    var x = '5'
+}
+
+printX();
+
+// Closures // способность функции запоминать переменные из контексат где она была создана
 
 
 
